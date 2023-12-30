d0b33c5a866726fedcd9499effff9cda5187abc0
Xu Ruozhou <328288351@qq.com>
2023-12-30 06:32:28 +0000

    12.30

---
+++ ae.h
@@ -0,0 +1,27 @@
+// AE template from OCB code https://www.cs.ucdavis.edu/~rogaway/ocb/performance/
+#ifndef _AE_H_
+#define _AE_H_
+typedef unsigned char u8;
+typedef struct _ae_ctx ae_ctx;
+
+ae_ctx *ae_allocate(void *misc);
+void ae_free(ae_ctx *ctx);
+
+int ae_init(ae_ctx *ctx,
+            const u8 *key,
+            int key_len,
+            int nonce_len,
+            int tag_len);
+
+int ae_encrypt(ae_ctx *ctx,
+               const u8 *nonce,
+               const u8 *pt,
+               int pt_len,
+               u8 *ct);
+
+int ae_decrypt(ae_ctx *ctx,
+               const u8 *nonce,
+               const u8 *ct,
+               int ct_len,
+               u8 *pt);
+#endif
---
+++ hctr_main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "aes.h"
+#include <x86intrin.h> //linux
+// #include <intrin.h>  //windows
+#include "ae.h"
+#include <stdlib.h>
+
+// linux
+void output(unsigned char *arr, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        printf("%02x", arr[i]);
+        if (i % 16 == 15)
+            printf("\n");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    unsigned int ui;
+    u8 key[32];
+    u8 pt[4096] = {0};
+    u8 pt2[4096] = {0};
+    u8 ct[4096] = {0};
+    ae_ctx *ctx = ae_allocate(NULL);
+    u8 tweak[16];
+    for (int i = 0; i < 16; i++)
+        tweak[i] = 0;
+    for (int i = 0; i < 32; i++)
+        key[i] = i;
+    for (int i = 0; i < 4096; i++)
+        pt[i] = rand();
+    ae_init(ctx, key, 32, 0, 0);
+    int pt_len=32;
+    output(pt,pt_len);
+    ae_encrypt(ctx, tweak, pt, pt_len, ct);
+    output(ct,pt_len);
+    ae_decrypt(ctx, tweak, ct, pt_len, pt2);
+    output(pt2,pt_len);
+    ae_free(ctx);
+    return 0;
+}
